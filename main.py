e1ced7b1b5140b8409785636c0f25ea4b1d1e846
dD4rky <nemodarky@gmail.com>
2024-03-04 21:19:38 +0300

    Pre Beta 0.2

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ import pandas as pd
 
 import os
 import json
+import asyncio
+from types import NoneType
 from time import sleep
 
 from pprint import pprint
@@ -18,7 +20,7 @@ def _obj_to_scalar(value: MoneyValue | Quotation):
     return value.units + value.nano / 1_000_000_000
 
 def _scalar_to_quotation(value : float):
-    return Quotation(units = value // 1, nano = (value % 1) * 1_000_000_000)
+    return Quotation(units = int(value), nano = int((value - int(value)) * 1_000_000_000))
 
 class Client(object):
     def __init__(self, token):
@@ -26,7 +28,6 @@ class Client(object):
         self.services = SandboxClient(self.token).__enter__()
         self.account = self._accounts()
 
-        positions = self.services.operations.get_portfolio(account_id=self.account).positions
 
     def _accounts(self):
         accounts = self.services.users.get_accounts().accounts
@@ -53,6 +54,7 @@ class Orders():
             if order.direction == OrderDirection.ORDER_DIRECTION_SELL:
                 self.orders[OrderDirection.ORDER_DIRECTION_SELL].append(order.order_id)
         return self.orders
+    
 class Stratagy():
     def __init__(self, _buy_cond, _sell_cond):
         self.buy_condition = _buy_cond
@@ -99,11 +101,13 @@ class TMOS_Stratagy(Stratagy):
     def sell_condition(self, client : Client, orders: Orders):
         orders = orders.update_orders(client)
         
-        for order in orders:
-            client.services.orders.cancel_order(account_id = client.account,
-                                                order_id = order)
+        for order in orders[OrderDirection.ORDER_DIRECTION_SELL]:
+            try:
+                client.services.orders.cancel_order(account_id = client.account,
+                                                    order_id = order)
+            except: pass
         position = self._get_position(client)
-        instrument = client.services.instruments.share_by(id_type = InstrumentIdType.INSTRUMENT_ID_TYPE_FIGI,
+        instrument = client.services.instruments.etf_by(id_type = InstrumentIdType.INSTRUMENT_ID_TYPE_FIGI,
                                                             id = position.figi).instrument
         if _obj_to_scalar(position.quantity) == 0:
             return
@@ -114,7 +118,7 @@ class TMOS_Stratagy(Stratagy):
                         direction = OrderDirection.ORDER_DIRECTION_SELL,
                         order_type = OrderType.ORDER_TYPE_LIMIT,
                         price = _scalar_to_quotation(price),
-                        quantity = _obj_to_scalar(position.quantity))
+                        quantity = int(_obj_to_scalar(position.quantity)))
         
     def _get_position(self, client : Client):
         positions = client.services.operations.get_portfolio(account_id = client.account).positions
@@ -123,17 +127,136 @@ class TMOS_Stratagy(Stratagy):
                 return position
         return PortfolioPosition(figi = 'BBG333333333',
                                  quantity = Quotation(0,0))
-if __name__ == "__main__": 
-    # get token
-    with open('./config.json', 'r') as f:
-        data  = json.load(f)
+    
+# if __name__ == "__main__": 
+#     # get token
 
-    os.environ['token'] = data['token']
 
-    client = Client(os.environ['token'])
-    orders = Orders(client)
-    str_ = TMOS_Stratagy()
-    while True:
-        if client.services.market_data.get_trading_status(figi='BBG333333333').trading_status == SecurityTradingStatus.SECURITY_TRADING_STATUS_NORMAL_TRADING:
-            str_.stratagy(client, orders)
-        sleep(0.5)
+
+#     orders = Orders(client)
+#     str_ = TMOS_Stratagy()
+#     while True:
+#         if client.services.market_data.get_trading_status(figi='BBG333333333').trading_status == SecurityTradingStatus.SECURITY_TRADING_STATUS_NORMAL_TRADING:
+#             str_.stratagy(client, orders)
+#         sleep(2)
+
+class Response():
+    def __init__(self, **kwargs):
+        for key, value in kwargs.items():
+            self.__dict__[key] = value
+
+class DataStorageResponse(Response):
+    def __init__(self, 
+                 positions : dict,
+                 orders : list,
+                 order_book : dict):
+        super().__init__(positions=positions, 
+                         orders=orders, 
+                         order_book=order_book)
+
+class Request():
+    def __init__(self, **kwargs):
+        for key, value in kwargs.items():
+            self.__dict__[key] = value
+
+class DataStorageRequest(Request):
+    def __init__(self, 
+                 figi : str,
+                 positions : bool = False,
+                 orders : bool = False,
+                 order_book : bool = False):
+        super().__init__(figi=figi, 
+                         positions=positions,
+                         orders=orders, 
+                         order_book=order_book)
+
+class DataManager():
+    def __init__(self,
+                 positions_state : bool = False,
+                 orders_state : bool = False,
+                 order_book : None | list = None):
+        
+        self.positions_state = positions_state
+
+        self.orders_state = orders_state
+
+        self.order_book = {}
+        if isinstance(order_book, NoneType):
+            return
+        for or_b in order_book:
+            self.order_book[or_b] = None
+
+    def update(self, client : Client) -> DataStorageResponse:
+        self._update(client)
+        return DataStorageResponse(self.positions, self.orders, self.order_book)
+    
+    def _update(self, client : Client) -> None: # protected
+        if self.orders_state:
+            self.orders = {OrderDirection.ORDER_DIRECTION_BUY : [],
+                           OrderDirection.ORDER_DIRECTION_SELL : []}
+            orders = client.services.orders.get_orders(account_id = client.account).orders
+            for order in orders:
+                if order.direction == OrderDirection.ORDER_DIRECTION_BUY:
+                    self.orders[OrderDirection.ORDER_DIRECTION_BUY].append(order)
+                if order.direction == OrderDirection.ORDER_DIRECTION_SELL:
+                    self.orders[OrderDirection.ORDER_DIRECTION_SELL].append(order)
+
+        if self.positions_state:
+            self.positions = client.services.operations.get_portfolio(account_id=client.account).positions
+
+        for or_b in self.order_book.keys():
+            self.order_book[or_b] = client.services.market_data.get_order_book(figi = or_b,
+                                                                    depth = 50)
+    
+    def get_data(self, request : DataStorageRequest):
+        if request.positions:
+            for pos in self.positions:
+                if pos.figi != request.figi:
+                    continue
+                positions = pos
+                break
+        if request.orders:
+            orders = {OrderDirection.ORDER_DIRECTION_BUY : [],
+                      OrderDirection.ORDER_DIRECTION_SELL : []}
+            for order in self.orders[OrderDirection.ORDER_DIRECTION_BUY]:
+                if order.figi == request.figi:
+                    orders[OrderDirection.ORDER_DIRECTION_BUY].append(order)
+            for order in self.orders[OrderDirection.ORDER_DIRECTION_SELL]:
+                if order.figi == request.figi:
+                    orders[OrderDirection.ORDER_DIRECTION_SELL].append(order)
+        if request.order_book:
+            order_book = self.order_book[request.figi]
+        return DataStorageResponse(positions=positions, orders=orders, order_book=order_book)
+    
+    def add_order_book(self, order_book : list | str):
+        if isinstance(order_book, list):
+            for or_b in order_book:
+                if or_b in self.order_book.keys():
+                    continue
+                self.order_book[or_b] = None
+        if or_b not in self.order_book.keys():
+            self.order_book[or_b] = None
+
+    def remove_order_book(self, order_book : list | str):
+        if isinstance(order_book, list):
+            for or_b in order_book:
+                if or_b not in self.order_book.keys():
+                    continue
+                del self.order_book[or_b]
+        if or_b in self.order_book.keys():
+            del self.order_book[or_b] 
+
+with open('./config.json', 'r') as f:
+    data  = json.load(f)
+
+os.environ['token'] = data['token']
+client = Client(os.environ['token'])
+
+
+dm = DataManager(positions_state = True, orders_state = True, order_book = ['BBG333333333'])
+pprint(dm.update(client).__dict__)
+
+pprint(dm.get_data(DataStorageRequest(figi='BBG333333333',
+                                     positions=True,
+                                     orders=True,
+                                     order_book=True)).orders)
